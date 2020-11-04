b0b51010f7ff789a84f981cdecbeb4e61ade2fa2
Kserlok <experiancullum@gmail.com>
2020-11-04 20:27:23 +1000

    ничего не работает и полностью другой код, извините((

---
+++ ОАИП/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ОАИП
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat("Кокос", new DateTime(2015, 02, 10));
+            Console.WriteLine($"Коту по имени {cat.Name} уже {cat.GetAge()} лет");
+            cat.HungryStatus = 150;
+            Console.ReadLine();
+        }
+    }
+}
